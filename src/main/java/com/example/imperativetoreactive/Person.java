0695474cc2cbcb672a29ae86a9ae1cf3d1620b6d
mbimbij <joseph.mbimbi@gmail.com>
2021-07-10 09:27:37 +0200

    Add Hibernate cache to be more or less iso with the start of the video

--- src/main/java/com/example/imperativetoreactive/Person.java
+++ src/main/java/com/example/imperativetoreactive/Person.java
@@ -4,15 +4,12 @@ import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.time.LocalDate;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "PERSON")
 @Data
+@Cacheable
 @Cache(region = "database", usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Person {
   @Id
--- src/main/java/com/example/imperativetoreactive/PersonController.java
+++ src/main/java/com/example/imperativetoreactive/PersonController.java
@@ -1,6 +1,7 @@
 package com.example.imperativetoreactive;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
