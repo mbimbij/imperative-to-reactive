c0bf49fb08403b489de5ed0b48e2e3009e87bab2
mbimbij <joseph.mbimbi@gmail.com>
2021-07-10 09:43:41 +0200

    Use WebMVC.fn instead of annotations

---
+++ src/main/java/com/example/imperativetoreactive/PersonRoutes.java
@@ -0,0 +1,29 @@
+package com.example.imperativetoreactive;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
+
+@Configuration
+public class PersonRoutes {
+  @Bean
+  public RouterFunction<ServerResponse> findAll(PersonRepository repository) {
+    return route().GET(
+        "/person",
+        req -> ok().body(repository.findAll(Sort.by("lastName", "firstName")))
+    ).build();
+  }
+
+  @Bean
+  public RouterFunction<ServerResponse> findById(PersonRepository repository) {
+    return route().GET(
+        "/person/{id}",
+        req -> ok().body(repository.findById(Long.valueOf(req.pathVariable("id"))))
+    ).build();
+  }
+}
